c553614dc1b180213d587536e016ca9ad8c125a5
Nicolle Romero <nicollearomero@gmail.com>
2020-08-05 17:38:35 -0700

    Completed Part I

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 """Server for movie ratings app."""
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 # Replace this with routes and view functions!
+@app.route("/")
+def homepage():
+    return render_template("base.html")
 
 
 if __name__ == '__main__':
